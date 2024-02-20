7d9e66f70943487e6f35b7344ba027ba77b7ee6b
mppl2226 <mppl2226@unipi.gr>
2024-02-20 22:40:49 +0200

    Main screen UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.unipiaudiostories"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.unipiaudiostories"
